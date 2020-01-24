44bce69d39cde04c7fa31cf4ca21bd1258c1432c
LauraAy <laura.ay1564@gmail.com>
2020-01-24 14:59:11 -0800

    Working

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,73 @@
-let length = +prompt('Choose a password length between 8 and 128 characters.')
+function generatePassword() {let length = +prompt('Choose a password length between 8 and 128 characters.')
 
-  if (length > 8 && length < 128) {
-    alert('You have chosen a password of ' + length + ' characters.')
-  }
-  else {
-    alert('Your password must be greater than 8 or less than 128 characters.')
+if (length > 8 && length < 128) {
+  alert('You have chosen a password of ' + length + ' characters.')
+}
+else {
+  alert('Your password must be greater than 8 or less than 128 characters.')
 
-  }
+}
 
-  var charChoice = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '1234567890', '{}()[]#:;^,.?!|&+-=*~_%@$']
+var charChoice = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '1234567890', '{}()[]#:;^,.?!|&+-=*~_%@$']
 
 
-  let prompt1 = prompt(`Do you want to use lower case letters? Type "yes" or "no."`)
+let prompt1 = prompt(`Do you want to use lower case letters? Type "yes" or "no."`)
 
-  let choice1 = ''
+let choice1 = ''
 
-    if (prompt1 === 'yes') {
-       choice1 = charChoice[0]
-    }
-    else {
-      choice1 = '\0'
-    }
+if (prompt1 === 'yes') {
+  choice1 = charChoice[0]
+}
+else {
+  choice1 = '\0'
+}
 
-  let prompt2 = prompt(`Do you want to use Upper case letters? Type "yes" or "no."`)
+let prompt2 = prompt(`Do you want to use Upper case letters? Type "yes" or "no."`)
 
-  let choice2 = ''
+let choice2 = ''
 
-  if (prompt2 === 'yes') {
-    choice2 = charChoice[1]
-  }
-  else {
-    choice2 = '\0'
-  }
+if (prompt2 === 'yes') {
+  choice2 = charChoice[1]
+}
+else {
+  choice2 = '\0'
+}
 
-  char = choice1 + choice2
+let prompt3 = prompt(`Do you want to use numbers? Type "yes" or "no."`)
 
-  console.log(char)
+let choice3 = ''
 
-  let password = '';
+if (prompt3 === 'yes') {
+  choice3 = charChoice[2]
+}
+else {
+  choice2 = '\0'
+}
 
-  for (var i = 0; i < length; i++){
-    password += char.charAt(Math.floor(Math.random() * length))
-  }
+let prompt4 = prompt(`Do you want to use special characters? Type "yes" or "no."`)
 
-  console.log(password)
+let choice4 = ''
+
+if (prompt4 === 'yes') {
+  choice4 = charChoice[3]
+}
+else {
+  choice2 = '\0'
+}
+
+char = choice1 + choice2 + choice3 + choice4
+
+console.log(char)
+
+let password = '';
+
+for (var i = 0, n = char.length; i < length; ++i) {
+  password += char.charAt(Math.floor(Math.random() * n))
+}
+
+  document.getElementById('password').textContent = password
+  
+}
 
 
 // Assignment Code
@@ -51,10 +75,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var password = generatePassword() 
 
 }
 
